6b1b9e9a39622f248631e06bdaf64ca693d4c4d3
José Saúl Rodríguez Guardado <rg18028@ues.edu.sv>
2024-05-06 22:20:44 -0600

    segundo mit
    deee

---
+++ src/main/java/com/IIS/RG18028/App.java
@@ -0,0 +1,17 @@
+package com.IIS.RG18028;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String Hello () {
+    	return " Hello World";
+    }
+    public static void  main(String []args)
+    {
+    	App hw = new App();
+        System.out.println( hw.Hello() );
+    }
+}
---
+++ src/test/java/com/IIS/RG18028/AppTest.java
@@ -0,0 +1,23 @@
+package com.IIS.RG18028;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+   
+
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
